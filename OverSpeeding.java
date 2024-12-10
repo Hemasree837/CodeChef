4b15888bb5fe4ceb9a6e9fc8f305b0b5072b7c43
Hemasree837 <hemasree837@gmail.com>
2024-12-10 13:46:23 +0530

    Add files via upload

---
+++ OverSpeeding.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class OverSpeeding
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		if(s>40) System.out.println("YES");
+		else System.out.println("NO");
+
+	}
+}
+
