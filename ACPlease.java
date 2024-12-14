f96fea88d77e4ee244e2572284553e8d18222c9d
Hemasree837 <hemasree837@gmail.com>
2024-12-14 16:02:17 +0530

    Create  ACPlease.java

---
+++ ACPlease.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ACPlease
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>30)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
