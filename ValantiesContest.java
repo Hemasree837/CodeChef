1edc83db636773b36900cc1e0a24b502757d8b75
Hemasree837 <hemasree837@gmail.com>
2024-12-14 15:44:23 +0530

    ValantiesContest.java

---
+++ ValantiesContest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ValantiesContest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N == 121) {
+            System.out.println("Likely");
+        } else {
+            System.out.println("Unlikely");
+        }
+
+	}
+}
