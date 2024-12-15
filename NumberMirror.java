e0d569be7e58e4dc866264fb975bf5b2efd39b9a
Hemasree837 <hemasree837@gmail.com>
2024-12-15 10:14:01 +0530

    Create NumberMirror.java

---
+++ NumberMirror.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+class NumberMirror
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+		
+		// Print the value of n
+		
+	}
+}
